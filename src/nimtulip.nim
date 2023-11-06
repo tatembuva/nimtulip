b32c4f9dfc109b46858074f229ef5493be3e2665
tatembuva <tatembuva92@gmail.com>
2023-11-07 01:49:12 +0200

    ...nimble config won't load after `nimble install`...replaced with static variable

--- src/nimtulip.nim
+++ src/nimtulip.nim
@@ -10,14 +10,16 @@ import nimtulip/tulip as ti
 include nimtulip/indicator_list
 
 # Globals(local)
-var prj: Config = loadConfig("./nimtulip.nimble")
+# var prj: Config = loadConfig("./nimtulip.nimble")
+var prj_ver: string = "0.1.0" 
+
 
 # Pretty printable [info] functions
 # Get Lib Version
 proc version*(pprint: bool = false): string =
   ## Returns current library version as a string.
   ## (pprint) arg pretty prints the lib header.
-  var nti = prj.getSectionValue("", "version") 
+  var nti = prj_ver 
   nti = "v" & nti
   var tulip_indicators = "v0.9.2"
   if(pprint):
@@ -32,7 +34,7 @@ proc version*(pprint: bool = false): string =
     styledEcho resetStyle, fgYellow, &"""  \__|  \__|""", fgGreen, &"""  \____/""", fgMagenta, &""" \__|""", styleDim, fgDefault, &""" 2023""", "\n"
     styledEcho resetStyle, fgDefault, "  repo    : ", resetstyle, styleUnderscore, fgblue, "https://github.com/tatembuva/nimtulip.git", resetStyle
     styledEcho fgDefault, "  version : ", resetStyle, fgCyan, $nti, resetStyle, styleDim, fgDefault, " [tulip indicators", " - ", $tulip_indicators, "]\n"
-  return "0.0.1" 
+  return "0.1.0" 
 
 proc indicators*(indicator: string = "", pprint: bool = false): seq[Indicator] =
   ## Returns list or single indicaor info
